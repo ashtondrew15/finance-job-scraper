584356eb93b2750001e9297d4e73fea5c8fef72d
ashtondrew15 <ashtondrew15@gmail.com>
2025-06-11 11:54:28 -0400

    Update __init__.py

--- scraping/__init__.py
+++ scraping/__init__.py
@@ -1,7 +1,1 @@
-diff --git a/scraping/__init__.py b/scraping/__init__.py
-index 5306933782c8fc8bd9509f7f4c6d0dce806623ce..eceac7bb190889570ff5591ced4fb9e960834e9f 100644
---- a/scraping/__init__.py
-+++ b/scraping/__init__.py
-@@ -1 +1 @@
--add __init__.py to fix import
-+# Job scraper package
+"""Job scraper package."""
